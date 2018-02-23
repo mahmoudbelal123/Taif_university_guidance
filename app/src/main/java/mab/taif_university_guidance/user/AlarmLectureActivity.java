295dbcf03a87fa760c50b46f5f1ef42416199f07
Osama <Oss2093@gmail.com>
2018-02-23 21:51:34 +0200

    Adding AlarmLectureActivity

---
+++ app/src/main/java/mab/taif_university_guidance/user/AlarmLectureActivity.java
@@ -0,0 +1,15 @@
+package mab.taif_university_guidance.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mab.taif_university_guidance.R;
+
+public class AlarmLectureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_lecture);
+    }
+}
