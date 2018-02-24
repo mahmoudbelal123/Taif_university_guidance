59fe551881e3a635e59fc58dea0a1bd2cb83b880
MahmoudBelal <mahmoudbelal955@gmail.com>
2018-02-24 14:53:39 +0200

    Member by Admin without update

---
+++ app/src/main/java/mab/taif_university_guidance/admin_view/members/UpdateMemberActivity.java
@@ -0,0 +1,15 @@
+package mab.taif_university_guidance.admin_view.members;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mab.taif_university_guidance.R;
+
+public class UpdateMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_member);
+    }
+}
