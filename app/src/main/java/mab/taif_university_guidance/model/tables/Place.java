e85ad5bd2cbaa4ee826967410492a8cd782cd40f
MahmoudBelal <mahmoudbelal955@gmail.com>
2018-03-04 09:45:57 +0200

    add place not ready now

---
+++ app/src/main/java/mab/taif_university_guidance/model/tables/Place.java
@@ -0,0 +1,16 @@
+package mab.taif_university_guidance.model.tables;
+
+/**
+ * Created by user on 3/4/2018.
+ */
+
+public class Place {
+
+    public static String ID_PLACE  = "id_place";
+    public static String NAME_PLACE = "name_place";
+    public static String IMAGE_PLACE = "image_place";
+    public static String IMAGE_LEVEL= "image_level";
+    public static String LOCATION = "location";
+    public static String DETAILS = "details";
+
+}
